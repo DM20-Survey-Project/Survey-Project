54cef0c786ffc154c3b9e9fb27381d4a248e6ae8
Mutethia Gatobu <evansgaji@gmail.com>
2017-05-02 12:40:32 -0600

    next one

--- public/dist/js/bundle.js
+++ public/dist/js/bundle.js
@@ -6,13 +6,11 @@ angular.module('surveyApp', ['ui.router']).config(function ($urlRouterProvider, 
 
   $stateProvider.state('user', {
     templateUrl: 'views/user.html',
-
     url: '/',
     controller: 'userCtrl'
 
   }).state('admin', {
     templateUrl: 'views/admin.html',
-
     url: '/admin',
     controller: 'adminCtrl'
 
